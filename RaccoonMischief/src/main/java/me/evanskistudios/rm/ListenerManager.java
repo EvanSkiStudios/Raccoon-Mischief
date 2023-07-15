105a1af2d3d63fe40c8af39fdc88fb2b6eabc244
Evanski <61751485+EvanSkiStudios@users.noreply.github.com>
2023-07-15 02:19:10 -0500

    bug fixes

--- RaccoonMischief/src/main/java/me/evanskistudios/rm/ListenerManager.java
+++ RaccoonMischief/src/main/java/me/evanskistudios/rm/ListenerManager.java
@@ -7,6 +7,7 @@ import me.evanskistudios.rm.Tasks.TaskRekkoRoomCode;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 
+
 public class ListenerManager {
     public static void registerEvent(org.bukkit.event.Listener listener) {
         Plugin namespace = RaccoonMischief.getPlugin();
@@ -41,3 +42,95 @@ public class ListenerManager {
         registerEvent( new ListenerEntitySpawn() ); //Entity spawn
     }
 }
+
+/*
+public class ListenerManager {
+    public static void Events() {
+        Plugin namespace = RaccoonMischief.getPlugin();
+
+        //CraftingRecipeInventoryListener
+        ComRMShowCraft InventoryListener = new ComRMShowCraft();
+        namespace.getServer().getPluginManager().registerEvents(InventoryListener, namespace);
+
+        //Lightning
+        ListenerLightning LightningEvent = new ListenerLightning();
+        namespace.getServer().getPluginManager().registerEvents(LightningEvent, namespace);
+
+        //PLayer Join
+        ListenerPlayerConnect PlayerConnectEvent = new ListenerPlayerConnect();
+        namespace.getServer().getPluginManager().registerEvents(PlayerConnectEvent, namespace);
+
+        //Player Death Event
+        ListenerPlayerDeath PlayerDeathEvent = new ListenerPlayerDeath(RaccoonMischief.getPlugin());
+        namespace.getServer().getPluginManager().registerEvents(PlayerDeathEvent, namespace);
+
+        //Milk potion
+        ListenerMilkPotion PotionSplashEvent = new ListenerMilkPotion();
+        namespace.getServer().getPluginManager().registerEvents(PotionSplashEvent, namespace);
+
+        //Player Right click
+        ListenerPlayerInteractEntity PlayerRightClickEvent = new ListenerPlayerInteractEntity();
+        namespace.getServer().getPluginManager().registerEvents(PlayerRightClickEvent, namespace);
+
+        //Crafting
+        ListenerPlayerCraft PlayerCraftEvent = new ListenerPlayerCraft();
+        namespace.getServer().getPluginManager().registerEvents(PlayerCraftEvent, namespace);
+
+        //Xp Crystal
+        ListenerXPCrystal PlayerTeleportEvent = new ListenerXPCrystal();
+        namespace.getServer().getPluginManager().registerEvents(PlayerTeleportEvent, namespace);
+
+        //teleport void
+        ListenerPlayerEnterVoid PlayerVoidEvent = new ListenerPlayerEnterVoid();
+        namespace.getServer().getPluginManager().registerEvents(PlayerVoidEvent, namespace);
+
+        //Entity fire
+        ListenerEntityFire EntityFireEvent = new ListenerEntityFire();
+        namespace.getServer().getPluginManager().registerEvents(EntityFireEvent, namespace);
+
+        //Player Inventory
+        ListenerPlayerInventory PlayerInventoryEvent = new ListenerPlayerInventory();
+        namespace.getServer().getPluginManager().registerEvents(PlayerInventoryEvent, namespace);
+
+        //Projectile Land
+        ListenerProjectileLand ProjectileLandEvent = new ListenerProjectileLand();
+        namespace.getServer().getPluginManager().registerEvents(ProjectileLandEvent, namespace);
+
+        //Telekinesis Enchant
+        EnchantListenerTelekinesis EnchantTelekinesisEvent = new EnchantListenerTelekinesis();
+        namespace.getServer().getPluginManager().registerEvents(EnchantTelekinesisEvent, namespace);
+
+        //GoatHorn
+        ListenerGoatHorn PlayerGoatHornEvent = new ListenerGoatHorn();
+        namespace.getServer().getPluginManager().registerEvents(PlayerGoatHornEvent, namespace);
+
+        //Rekko
+        TaskRekkoRoomCode ListenerRekkoJoin = new TaskRekkoRoomCode(RaccoonMischief.getPlugin());
+        namespace.getServer().getPluginManager().registerEvents(ListenerRekkoJoin, namespace);
+
+        //Right click block
+        ListenerPlayerInteractBlock PlayerUseBlockEvent = new ListenerPlayerInteractBlock();
+        namespace.getServer().getPluginManager().registerEvents(PlayerUseBlockEvent, namespace);
+
+        //Item despawn
+        ListenerItemDespawn ItemDespawnEvent = new ListenerItemDespawn();
+        namespace.getServer().getPluginManager().registerEvents(ItemDespawnEvent, namespace);
+
+        //Mob Spawning
+        ListenerMobSpawn MobSpawnEvent = new ListenerMobSpawn(RaccoonMischief.getPlugin());
+        namespace.getServer().getPluginManager().registerEvents(MobSpawnEvent, namespace);
+
+        //Placing
+        ListenerPlayerPlaceItem PlaceItemEvent = new ListenerPlayerPlaceItem();
+        namespace.getServer().getPluginManager().registerEvents(PlaceItemEvent, namespace);
+
+        //Cake
+        ListenerPlayerBreakBlock BreakBlockEvent = new ListenerPlayerBreakBlock();
+        namespace.getServer().getPluginManager().registerEvents(BreakBlockEvent, namespace);
+
+        //PlayerRespawnPotionEffects
+        ListenerPlayerRespawn PlayerRespawnEvent = new ListenerPlayerRespawn();
+        namespace.getServer().getPluginManager().registerEvents(PlayerRespawnEvent, namespace);
+    }
+}
+ */
--- RaccoonMischief/src/main/java/me/evanskistudios/rm/Listeners/ListenerMobSpawn.java
+++ RaccoonMischief/src/main/java/me/evanskistudios/rm/Listeners/ListenerMobSpawn.java
@@ -1,6 +1,7 @@
 package me.evanskistudios.rm.Listeners;
 
 import me.evanskistudios.rm.RaccoonMischief;
+import org.bukkit.Effect;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
@@ -9,6 +10,9 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.potion.PotionType;
+
+import java.util.Collection;
 
 import static me.evanskistudios.rm.Utilis.UtilityMethods.Choose;
 
@@ -24,12 +28,12 @@ public class ListenerMobSpawn implements Listener {
         Entity SpawnedEntity = event.getEntity();
 
         if (SpawnedEntity instanceof Creeper Crep){
-            if (( Crep.getWorld().getBlockAt(Crep.getLocation()).getLightFromSky() ) >= 15) {
-                PotionEffect Effect = new PotionEffect((PotionEffectType) Choose(PotionEffects), 99999, 3);
+           // if (( Crep.getWorld().getBlockAt(Crep.getLocation()).getLightFromSky() ) >= 15) {
+                PotionEffect Effect = new PotionEffect((PotionEffectType) Choose(PotionEffects), 9600, 3);
                 Crep.addPotionEffect(Effect);
                 Crep.setPowered(true);
                 //Crep.getWorld().strikeLightning(Crep.getLocation());
-            }
+            //}
         }
     }
 
@@ -38,19 +42,20 @@ public class ListenerMobSpawn implements Listener {
         Entity DamagedEntity = event.getEntity();
 
         if (DamagedEntity instanceof Creeper Crep){
-            EntityDamageEvent.DamageCause DamageCause = event.getCause();
+            //EntityDamageEvent.DamageCause DamageCause = event.getCause();
+            event.setCancelled(true);
 
             // accidentally had this inverted, so it only let lightning and fire damage creepers
-            if ( (DamageCause == EntityDamageEvent.DamageCause.LIGHTNING) || (DamageCause == EntityDamageEvent.DamageCause.FIRE)){
-                event.setCancelled(true);
-            }
+//            if ( (DamageCause == EntityDamageEvent.DamageCause.LIGHTNING) || (DamageCause == EntityDamageEvent.DamageCause.FIRE)){
+//                event.setCancelled(true);
+//            }
             // Make creepers always explode
-            // if (DamageCause == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION){
-                Crep.setExplosionRadius(12);
+//            if (DamageCause == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION){
+//                Crep.setExplosionRadius(12);
                 Crep.setMaxFuseTicks(1);
                 Crep.setFuseTicks(1);
                 Crep.ignite();
-            // }
+//            }
         }
     }
 }
